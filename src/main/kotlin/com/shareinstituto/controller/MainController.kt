d9c7b29f7aa6f9da3f8e6e709c6b285fe3dddf3e
Renan Oliveira <renan04lima@outlook.com>
2019-10-05 21:14:21 -0300

    login

--- src/main/kotlin/com/shareinstituto/controller/MainController.kt
+++ src/main/kotlin/com/shareinstituto/controller/MainController.kt
@@ -30,7 +30,7 @@ class MainController : EndpointGroup {
     }
 
     fun login(ctx: Context) {
-        LoginView(dao).rend
+        LoginView(dao).render(ctx)
     }
     //fun page(ctx: Context) {
     //
