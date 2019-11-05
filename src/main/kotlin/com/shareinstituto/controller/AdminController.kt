414554c27e9ed89438d04b8ff00ab178b013f114
Renan Oliveira <renan04lima@outlook.com>
2019-11-05 16:51:58 -0300

    editarLinkView

--- src/main/kotlin/com/shareinstituto/controller/AdminController.kt
+++ src/main/kotlin/com/shareinstituto/controller/AdminController.kt
@@ -3,6 +3,8 @@ package com.shareinstituto.controller
 import com.shareinstituto.controller.security.MainRole.ADMIN
 import com.shareinstituto.model.dao.DataAccessObject
 import com.shareinstituto.view.AdminView
+import com.shareinstituto.view.EditarLinkView
+import com.shareinstituto.view.EditarNoticiaView
 import com.shareinstituto.view.EditarPaginaView
 import io.javalin.apibuilder.ApiBuilder.get
 import io.javalin.apibuilder.EndpointGroup
@@ -17,14 +19,41 @@ class AdminController(override val kodein: Kodein) : EndpointGroup, KodeinAware 
 
     override fun addEndpoints() {
         get(::admin, roles(ADMIN))
-        get("note", ::note, roles(ADMIN))
+        get("novaPagina", ::novaPagina, roles(ADMIN))
+        get("novaNoticia", ::novaNoticia, roles(ADMIN))
+        get("editarPagina/:pagina", ::editarPagina, roles(ADMIN))
+        get("editarNoticia/:noticia", ::editarNoticia, roles(ADMIN))
+        get("novoLink", ::novoLink, roles(ADMIN))
+        get("editarLink/:link", ::editarLink, roles(ADMIN))
     }
 
     fun admin(ctx: Context) {
         AdminView(dao).render(ctx)
     }
 
-    fun note(ctx: Context) {
+    fun novaPagina(ctx: Context) {
         EditarPaginaView(dao).render(ctx)
     }
+
+    fun novaNoticia(ctx: Context) {
+        EditarNoticiaView(dao).render(ctx)
+    }
+
+    fun editarPagina(ctx: Context) {
+        EditarPaginaView(dao).render(ctx)
+    }
+
+    fun editarNoticia(ctx: Context) {
+        EditarNoticiaView(dao).render(ctx)
+    }
+
+    fun novoLink(ctx: Context) {
+        EditarLinkView(dao).render(ctx)
+    }
+
+    fun editarLink(ctx: Context) {
+        EditarLinkView(dao).render(ctx)
+    }
+
+
 }
