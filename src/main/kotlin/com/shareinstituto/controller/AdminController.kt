a9d46d58aab8b95f85934c7466a9525f4eb108d5
adriantodt <adrian.todtsilva@gmail.com>
2019-11-05 18:29:42 -0300

    Adicionado esqueleto de todos os endpoints

--- src/main/kotlin/com/shareinstituto/controller/AdminController.kt
+++ src/main/kotlin/com/shareinstituto/controller/AdminController.kt
@@ -7,6 +7,7 @@ import com.shareinstituto.view.EditarLinkView
 import com.shareinstituto.view.EditarNoticiaView
 import com.shareinstituto.view.EditarPaginaView
 import io.javalin.apibuilder.ApiBuilder.get
+import io.javalin.apibuilder.ApiBuilder.post
 import io.javalin.apibuilder.EndpointGroup
 import io.javalin.core.security.SecurityUtil.roles
 import io.javalin.http.Context
@@ -19,12 +20,28 @@ class AdminController(override val kodein: Kodein) : EndpointGroup, KodeinAware 
 
     override fun addEndpoints() {
         get(::admin, roles(ADMIN))
+
         get("novaPagina", ::novaPagina, roles(ADMIN))
+        post("novaPagina", ::criarNovaPagina, roles(ADMIN))
+
         get("novaNoticia", ::novaNoticia, roles(ADMIN))
-        get("editarPagina/:pagina", ::editarPagina, roles(ADMIN))
-        get("editarNoticia/:noticia", ::editarNoticia, roles(ADMIN))
+        post("novaNoticia", ::criarNovaNoticia, roles(ADMIN))
+
         get("novoLink", ::novoLink, roles(ADMIN))
+        post("novoLink", ::criarNovoLink, roles(ADMIN))
+
+        get("editarPagina/:pagina", ::editarPagina, roles(ADMIN))
+        post("editarPagina/:pagina", ::salvarEdicaoPagina, roles(ADMIN))
+
+        get("editarNoticia/:noticia", ::editarNoticia, roles(ADMIN))
+        post("editarNoticia/:noticia", ::salvarEdicaoNoticia, roles(ADMIN))
+
         get("editarLink/:link", ::editarLink, roles(ADMIN))
+        post("editarLink/:link", ::salvarEdicaoLink, roles(ADMIN))
+
+        get("removerPagina/:pagina", ::removerPagina, roles(ADMIN))
+        get("removerNoticia/:noticia", ::removerNoticia, roles(ADMIN))
+        get("removerLink/:link", ::removerLink, roles(ADMIN))
     }
 
     fun admin(ctx: Context) {
@@ -35,25 +52,59 @@ class AdminController(override val kodein: Kodein) : EndpointGroup, KodeinAware 
         EditarPaginaView(dao).render(ctx)
     }
 
+    fun criarNovaPagina(ctx: Context) {
+
+    }
+
     fun novaNoticia(ctx: Context) {
         EditarNoticiaView(dao).render(ctx)
     }
 
-    fun editarPagina(ctx: Context) {
-        EditarPaginaView(dao).render(ctx)
-    }
+    fun criarNovaNoticia(ctx: Context) {
 
-    fun editarNoticia(ctx: Context) {
-        EditarNoticiaView(dao).render(ctx)
     }
 
     fun novoLink(ctx: Context) {
         EditarLinkView(dao).render(ctx)
     }
 
+    fun criarNovoLink(ctx: Context) {
+
+    }
+
+    fun editarPagina(ctx: Context) {
+        EditarPaginaView(dao).render(ctx)
+    }
+
+    fun salvarEdicaoPagina(ctx: Context) {
+
+    }
+
+    fun editarNoticia(ctx: Context) {
+        EditarNoticiaView(dao).render(ctx)
+    }
+
+    fun salvarEdicaoNoticia(ctx: Context) {
+
+    }
+
     fun editarLink(ctx: Context) {
         EditarLinkView(dao).render(ctx)
     }
 
+    fun salvarEdicaoLink(ctx: Context) {
 
+    }
+
+    fun removerPagina(ctx: Context) {
+
+    }
+
+    fun removerNoticia(ctx: Context) {
+
+    }
+
+    fun removerLink(ctx: Context) {
+
+    }
 }
