4de580e0da0fda0c5784ac747f2ffc64372cd49d
adriantodt <adrian.todtsilva@gmail.com>
2019-11-05 16:32:44 -0300

    Views de Edição Funcionando

--- src/main/kotlin/com/shareinstituto/view/EditarNoticiaView.kt
+++ src/main/kotlin/com/shareinstituto/view/EditarNoticiaView.kt
@@ -3,48 +3,46 @@ package com.shareinstituto.view
 import com.shareinstituto.model.dao.DataAccessObject
 import io.javalin.http.Context
 import kotlinx.html.*
+import kotlinx.html.ButtonType.submit
 
 class EditarNoticiaView(dao: DataAccessObject) : AdminModeloView(dao) {
-    override val pageTitle = "Summernote Exemplo"
+    override val pageTitle = "Nova Notícia"
     override val extraCss = listOf("/css/administração.css")
     override val mainPage = "/admin"
 
     override fun MAIN.renderMain(ctx: Context) {
-        div(classes = "summernote")
-        form(method = FormMethod.post) {
-            div("row") {
-                div(classes = "input-field col s8") {
-                    input(type = InputType.text, classes = "validate") {
-                        placeholder = "Título"
-                        id = "inputTitle"
+        div("container") {
+            h5("underlined") { +"Nova Notícia" }
+            form(method = FormMethod.post) {
+                div("row") {
+                    div(classes = "input-field col s12") {
+                        label {
+                            htmlFor = "inputTitle"
+                            +"Título da notícia"
+                        }
+                        input(type = InputType.text, classes = "validate") {
+                            placeholder = "Título"
+                            id = "inputTitle"
+                            name = "title"
+                        }
                     }
-                    label {
-                        htmlFor = "inputTitle"
 
-                        +"Título da Página"
+                    div("col s12") {
+                        label {
+                            +"Corpo da página"
+                        }
+                        textArea {
+                            id = "summernote"
+                            name = "html"
+                        }
                     }
-                }
-                div(classes = "input-field col s4") {
-                    input(type = InputType.text, classes = "validate") {
-                        placeholder = "Link da Página"
-                        id = "inputTitle"
+
+                    div("col s12 input-field") {
+                        button(classes = "btn waves-effect light-blue lighten-2", type = submit) {
+                            +"Criar Notícia"
+                            i("material-icons right") { +"send" }
+                        }
                     }
-                    label {
-                        htmlFor = "inputTitle"
-
-                        +"Título da Página"
-                    }
-                }
-            }
-
-            input() {
-                id = "summernote"
-            }
-
-            button(classes = "btn waves-effect waves-light", type = ButtonType.submit, name = "action") {
-                +"Submit"
-                i("material-icons right") {
-                    +"send"
                 }
             }
         }
@@ -52,16 +50,17 @@ class EditarNoticiaView(dao: DataAccessObject) : AdminModeloView(dao) {
 
     override fun HEAD.extraLinks() {
         link(href = "https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-lite.css", rel = "stylesheet")
-        script(src = "https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-lite.js") {}
     }
 
     override fun BODY.extraScripts() {
         script(src = "http://code.jquery.com/jquery-3.4.1.min.js") {}
+        script(src = "https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-lite.js") {}
+        script(src = "https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/lang/summernote-pt-BR.min.js") {}
         script {
             unsafe {
                 +"""
                 $(document).ready(function() {
-                    $('#summernote').summernote();
+                    $('#summernote').summernote({ height: 400, lang: 'pt-BR' });
                 })
                 """.trimIndent()
             }
--- src/main/kotlin/com/shareinstituto/view/EditarPaginaView.kt
+++ src/main/kotlin/com/shareinstituto/view/EditarPaginaView.kt
@@ -3,37 +3,79 @@ package com.shareinstituto.view
 import com.shareinstituto.model.dao.DataAccessObject
 import io.javalin.http.Context
 import kotlinx.html.*
+import kotlinx.html.ButtonType.submit
 
 class EditarPaginaView(dao: DataAccessObject) : AdminModeloView(dao) {
-    override val pageTitle = "Summernote Exemplo"
+    override val pageTitle = "Nova Página"
     override val extraCss = listOf("/css/administração.css")
     override val mainPage = "/admin"
 
     override fun MAIN.renderMain(ctx: Context) {
-        div(classes = "summernote")
-        form(method = FormMethod.post) {
-            input() {
-                id = "summernote"
+        div("container") {
+            h5("underlined") { +"Nova Página" }
+            form(method = FormMethod.post) {
+                div("row") {
+                    div(classes = "input-field col s8") {
+                        label {
+                            htmlFor = "inputTitle"
+                            +"Título da página"
+                        }
+                        input(type = InputType.text, classes = "validate") {
+                            placeholder = "Título"
+                            id = "inputTitle"
+                            name = "title"
+                        }
+                    }
+
+                    div(classes = "input-field col s4") {
+                        label {
+                            htmlFor = "inputLink"
+                            +"Link da página"
+                        }
+                        input(type = InputType.text, classes = "validate") {
+                            placeholder = "/link_da_pagina"
+                            id = "inputLink"
+                            name = "linkPagina"
+                        }
+                    }
+
+                    div("col s12") {
+                        label {
+                            +"Corpo da página"
+                        }
+                        textArea {
+                            id = "summernote"
+                            name = "html"
+                        }
+                    }
+
+                    div("col s12 input-field") {
+                        button(classes = "btn waves-effect light-blue lighten-2", type = submit) {
+                            +"Criar Página"
+                            i("material-icons right") { +"send" }
+                        }
+                    }
+                }
             }
         }
     }
 
     override fun HEAD.extraLinks() {
         link(href = "https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-lite.css", rel = "stylesheet")
-        script(src = "https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-lite.js") {}
     }
 
     override fun BODY.extraScripts() {
         script(src = "http://code.jquery.com/jquery-3.4.1.min.js") {}
+        script(src = "https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-lite.js") {}
+        script(src = "https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/lang/summernote-pt-BR.min.js") {}
         script {
             unsafe {
                 +"""
                 $(document).ready(function() {
-                    $('#summernote').summernote();
+                    $('#summernote').summernote({ height: 400, lang: 'pt-BR' });
                 })
                 """.trimIndent()
             }
         }
     }
 }
-//
